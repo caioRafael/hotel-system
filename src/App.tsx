9690c42a5629140ca40c41e72c7b294ed3934eb9
caioRafael <caiorafaelrg@gmail.com>
2023-10-12 16:31:46 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+  return (
+    <div>
+      <h1>ola mundo</h1>
+      <Button>
+        Caio
+      </Button>
+    </div>
+  )
+}
+
+export default App
